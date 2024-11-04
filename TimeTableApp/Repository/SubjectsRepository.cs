0b8dd8a9a89033b72c76df0b5fdc34702cbc714a
Maria Felecan <mariafelecan19@gmail.com>
2024-11-04 17:32:40 +0200

    push nebun

---
+++ TimeTableApp/Repository/SubjectsRepository.cs
@@ -0,0 +1,38 @@
+using TimeTableApp.Models;
+
+namespace TimeTableApp.Repository;
+
+public class SubjectsRepository
+{
+    private List<Subject> SubjectsList;
+
+    public SubjectsRepository()
+    {
+        SubjectsList = new List<Subject>();
+        SubjectsList.Add(new Subject(30, "mate"));
+        SubjectsList.Add(new Subject(30, "romana"));
+        SubjectsList.Add(new Subject(50, "engleza"));
+        SubjectsList.Add(new Subject(15, "chimie"));
+        SubjectsList.Add(new Subject(30, "georgrafie"));
+        SubjectsList.Add(new Subject(30, "istorie"));
+        SubjectsList.Add(new Subject(30, "educatie civica"));
+        SubjectsList.Add(new Subject(30, "sport"));
+        SubjectsList.Add(new Subject(28, "germana"));
+        SubjectsList.Add(new Subject(31, "franceza"));
+    }
+
+    public Guid GetSubjectGuid(String name)
+    {
+        foreach (Subject subject in SubjectsList )
+        {
+            if (subject.name == name)
+                return subject._id;
+        }
+
+        return Guid.Empty;
+    }
+    public void AddSubject(Subject subject)
+    {
+        SubjectsList.Add(subject);
+    }
+}
---
+++ TimeTableApp/Repository/TeachersRepository.cs
@@ -0,0 +1,49 @@
+using TimeTableApp.Models;
+
+namespace TimeTableApp.Repository;
+
+public class TeachersRepository
+{
+    private List<Teacher> TeachersList;
+    private SubjectsRepository SubjectsRepository;
+
+    TeachersRepository()
+    {
+        TeachersList = new List<Teacher>();
+        
+        List<Guid> TeacherSubjectList1 = new List<Guid>();
+        TeacherSubjectList1.Add(SubjectsRepository.GetSubjectGuid("romana"));
+        TeacherSubjectList1.Add(SubjectsRepository.GetSubjectGuid("mate"));
+        
+        TeachersList.Add(new Teacher("delia", TeacherSubjectList1 ));
+        
+        List<Guid> teacherSubjectList2 = new List<Guid>
+        {
+            SubjectsRepository.GetSubjectGuid("engleza"),
+            SubjectsRepository.GetSubjectGuid("germana")
+        };
+        TeachersList.Add(new Teacher("Ion", teacherSubjectList2));
+
+        List<Guid> teacherSubjectList3 = new List<Guid>
+        {
+            SubjectsRepository.GetSubjectGuid("istorie"),
+            SubjectsRepository.GetSubjectGuid("educatie civica"),
+            SubjectsRepository.GetSubjectGuid("romana")
+        };
+        TeachersList.Add(new Teacher("Maria", teacherSubjectList3));
+
+        List<Guid> teacherSubjectList4 = new List<Guid>
+        {
+            SubjectsRepository.GetSubjectGuid("geografie"),
+            SubjectsRepository.GetSubjectGuid("germana"),
+            SubjectsRepository.GetSubjectGuid("sport")
+        };
+        TeachersList.Add(new Teacher("Andrei", teacherSubjectList4));
+
+    }
+
+    public void AddTeacher(Teacher teacher)
+    {
+        TeachersList.Add(teacher);
+    }
+}
