779909249f955fc37dd788adca5cbfe489c0afc7
Maria Felecan <mariafelecan19@gmail.com>
2024-11-04 17:16:13 +0200

    ce se intampla?

--- TimeTableApp/Models/Subject.cs
+++ TimeTableApp/Models/Subject.cs
@@ -1,14 +1,14 @@
-namespace TimeTableApp
+namespace TimeTableApp.Models
 {
-    class Subject
+    public class Subject
     {
-        private Guid _id { get; set; }
+        public Guid _id { get; set; }
         private int nrCopii { get; set; }
         private string name { get; set; }
         private bool specificRoom {  get; set; }
         private Guid? roomId {  get; set; }
 
-        Subjects(int nr, string name, Guid roomId)
+        public Subject(int nr, string name, Guid roomId)
         {
             _id = Guid.NewGuid();
             this.nrCopii = nr;
@@ -16,7 +16,7 @@ namespace TimeTableApp
             this.specificRoom = true;
             this.roomId = roomId;
         }
-        Subjects(int nr, string name)
+        public Subject(int nr, string name)
         {
             _id = Guid.NewGuid();
             this.nrCopii = nr;
--- TimeTableApp/Models/Teacher.cs
+++ TimeTableApp/Models/Teacher.cs
@@ -1,24 +1,24 @@
 namespace TimeTableApp.Models;
 
-public class Teachers
+public class Teacher
 {
     private string teacherName{get;set;}
     private Guid teacherId{get;set;}
     private List<Guid> teacherSubjects{get;set;}
 
-    Teachers(string teacherName, Guid teacherId, List<Guid> teacherSubjects)
+    public Teacher(string teacherName, List<Guid> teacherSubjects)
     {
         this.teacherName = teacherName;
         this.teacherId = Guid.NewGuid();
         this.teacherSubjects = teacherSubjects;
     }
 
-    public void addNewSubjectForTeacher(Guid subjectId)
+    public void AddNewSubjectForTeacher(Guid subjectId)
     {
         this.teacherSubjects.Add(subjectId);
     }
 
-    public Boolean doesTeacherTeachSubject(Guid subjectId)
+    public Boolean DoesTeacherTeachSubject(Guid subjectId)
     {
         foreach (Guid id in teacherSubjects)
         {
